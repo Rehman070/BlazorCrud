6eb41270297c63eb315874030d5fac5d6b7bcc68
Abdur Rehman <abdurrehman5732@gmail.com>
2025-05-11 10:34:15 +0500

    Add project files.

---
+++ BlazorCrud.Server.Api/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlazorCrud.Library;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Server.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
